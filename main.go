97e2d691b316ab5d8d1feea7ebb9f151a2b2fb17
AtomAlpaca <atal@anche.no>
2025-07-18 20:10:24 +0800

    Init

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "log"
+
+func main() {
+	// Initialize the P2P client and other components here
+	// This is just a placeholder for the main function
+	log.Println("P2P Agent is running...")
+}
